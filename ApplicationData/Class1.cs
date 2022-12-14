35ad78ddfa8cdbcb09586c86bf82dffda4d95598
u31504 <u31504@P31504>
2022-12-14 12:40:10 +1000

    ktyd

---
+++ ApplicationData/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_ISRPO_Samkova_PR12.ApplicationData
+{
+    class Class1
+    {
+
+    }
+}
